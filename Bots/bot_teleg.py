d39a10c2f5889c7bac51b65a68192c8b05d87b8a
FHOFHAWK <50171653+FHOFHAWK@users.noreply.github.com>
2020-07-17 12:17:27 +0700

    Исправил импорты из папок

---
+++ Bots/bot_teleg.py
@@ -0,0 +1,41 @@
+import telebot
+from Server import server
+from telebot import types
+from System.constants import TELEGRAM_BOT_TOKEN
+
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+change_channel_btn = types.KeyboardButton('Сменить канал коммуникации')
+markup.add(change_channel_btn)
+
+
+class Bot(object):
+    bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+    # Метод для вызова отправки сообщения ботом в VK из других платформ
+    def send_new_mes(self, user_id):
+        self.bot.send_message(user_id, 'Приветствую в выбранном канале коммуникации!', reply_markup=markup)
+
+    def __init__(self):
+        # Создание бота через токен
+        @self.bot.message_handler(content_types=['text'])
+        def send_message(message):
+
+            # Если get_answer_from_server() что-то вернул из БД, то ответ
+            answer = server.get_answer_from_server(message.text.lower(), message.from_user.id, 'teleg')
+
+            if answer:
+                self.bot.send_message(message.from_user.id, answer, reply_markup=markup)
+            else:  # иначе...
+                self.bot.send_message(message.from_user.id,
+                                      "Я тебя не понимаю, не могу ответить на твой вопрос. Напиши /help.",
+                                      reply_markup=markup)
+
+        self.bot.polling()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ Bots/bot_vk.py
@@ -0,0 +1,50 @@
+import vk_api
+from Server import server
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from System.constants import VK_BOT_TOKEN
+
+keyboard = VkKeyboard(one_time=False)
+keyboard.add_button('Сменить канал коммуникации', color=VkKeyboardColor.PRIMARY)
+
+
+class Bot(object):
+    vk_session = vk_api.VkApi(
+        token=VK_BOT_TOKEN)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+
+    def __init__(self):
+        # Если пришло новое сообщение боту
+        for event in self.longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.to_me:
+
+                    message = event.text.lower()  # сохраняем полученное сообщение
+                    id = event.user_id  # сохраняем id
+
+                    # отправляем сообщение, id, тип платформы серверу на обработку
+                    answer = server.get_answer_from_server(message, id, 'vk')
+
+                    # Если get_answer_from_server() что-то вернул из БД, то
+                    if answer:
+                        self.vk_session.method('messages.send', {'user_id': id, 'message': answer, 'random_id': 0,
+                                                                 'keyboard': keyboard.get_keyboard()})
+                    else:
+                        answer = 'Я тебя не понимаю, не могу ответить на твой вопрос. Напиши /help.'
+                        self.vk_session.method('messages.send', {'user_id': id, 'message': answer, 'random_id': 0,
+                                                                 'keyboard': keyboard.get_keyboard()})
+
+    # Метод для вызова отправки сообщения ботом в VK из других платформ
+    def send_new_mes(self, user_id):
+        self.vk_session.method('messages.send',
+                               {'user_id': user_id, 'message': 'Приветствую в выбранном канале коммуникации!',
+                                'random_id': 0, 'keyboard': keyboard.get_keyboard()})
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
